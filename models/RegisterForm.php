dfd1ff359ce774c6c8f90fb5e25c0e1a3094c53b
naumaanalvi <malvi9322@gmail.com>
2019-09-19 16:12:32 +0530

    first commit

---
+++ models/RegisterForm.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+use yii\base\model;
+
+class RegisterForm extends Model
+{
+	public $name;
+	public $email;
+	public $contact;
+	public $password;
+	public $rememberMe = true;
+	
+	private $_user = false;
+	
+	public function rules()
+	{
+		return [
+			[['name', 'email', 'contact', 'password'], 'required'],
+			['rememberMe', 'boolean'],
+			['password', 'validatePassword'],
+		];
+	}
+	public function validatePassword($attribute, $params)
+    {
+        if (!$this->hasErrors()) {
+            $user = $this->getUser();
+
+            if (!$user || !$user->validatePassword($this->password)) {
+                $this->addError($attribute, 'Incorrect username or password.');
+            }
+        }
+    }
+	
+}
