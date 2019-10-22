c2f71bfa4fbad0dc4f1fb03fc25d0ecfe3199083
vishal.bansode055@gmail.com <vishal.bansode055@gmail.com>
2019-10-22 10:50:31 +0530

    done

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2',
+    'dsn' => 'mysql:host=localhost;dbname=electric_trade',
     'username' => 'root',
     'password' => '',
     'charset' => 'utf8',
