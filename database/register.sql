4bb22fc4704a6142612f81afd4cad8200dda44e9
naumaanalvi <malvi9322@gmail.com>
2019-10-28 12:01:28 +0530

    done

--- database/register.sql
+++ database/register.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 25, 2019 at 04:44 PM
+-- Generation Time: Oct 28, 2019 at 07:22 AM
 -- Server version: 10.3.16-MariaDB
 -- PHP Version: 7.2.19
 
@@ -40,12 +40,13 @@ CREATE TABLE `register` (
   `contact` int(15) NOT NULL,
   `email` varchar(300) NOT NULL,
   `password` varchar(300) NOT NULL,
+  `confirmPassword` varchar(255) NOT NULL,
   `create_date` date NOT NULL,
   `update_date` datetime NOT NULL DEFAULT current_timestamp(),
   `latchOn` varchar(255) NOT NULL,
   `iPartner` tinyint(1) NOT NULL,
   `catalouge` varchar(255) NOT NULL,
-  `street` varchar(255) NOT NULL,
+  `address` varchar(255) NOT NULL,
   `city` varchar(255) NOT NULL,
   `zip` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -54,8 +55,8 @@ CREATE TABLE `register` (
 -- Dumping data for table `register`
 --
 
-INSERT INTO `register` (`user_id`, `code`, `fName`, `lName`, `orgName`, `gst`, `registerAs`, `iecode`, `contact`, `email`, `password`, `create_date`, `update_date`, `latchOn`, `iPartner`, `catalouge`, `street`, `city`, `zip`) VALUES
-(1, '', 'jsd', 'skd', 'ajhsd', '', 'Manufacturer', 'cvbnm,.', 2147483647, 'jhahsdjks@sd.com', 'asdfghjkl', '0000-00-00', '2019-10-25 16:05:02', 'vbnmkl', 1, '', 'slkdjsdkl', 'sldkfjksdfl', '400043');
+INSERT INTO `register` (`user_id`, `code`, `fName`, `lName`, `orgName`, `gst`, `registerAs`, `iecode`, `contact`, `email`, `password`, `confirmPassword`, `create_date`, `update_date`, `latchOn`, `iPartner`, `catalouge`, `address`, `city`, `zip`) VALUES
+(1, '', 'jsd', 'skd', 'ajhsd', '', 'Manufacturer', 'cvbnm,.', 2147483647, 'jhahsdjks@sd.com', 'asdfghjkl', '', '0000-00-00', '2019-10-25 16:05:02', 'vbnmkl', 1, '', 'slkdjsdkl', 'sldkfjksdfl', '400043');
 
 --
 -- Indexes for dumped tables
